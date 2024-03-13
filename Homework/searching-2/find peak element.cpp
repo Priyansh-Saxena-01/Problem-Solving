407bca91238b12a9acc51869e96cd6b7001e4666
Priyansh-Saxena-01 <satyendrasaxena9876@gmail.com>
2024-03-13 10:51:23 +0530

    commit

--- Homework/searching-2/find peak element.cpp
+++ Homework/searching-2/find peak element.cpp
@@ -2,29 +2,22 @@
 using namespace std;
 
 int peakele(vector<int>nums){
-    int low=0;
-    int high=nums.size()-1;
-    if(high==0) return nums[high];
+        int n=nums.size();
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
 
-    while(low<=high){
-        int mid=low+(high-low)/2;
+        int low=0;
+        int high=n-1;
 
-        if(mid-1<0 && nums[mid]>=nums[mid+1]) return mid;
-        else if(mid-1<0 && nums[mid]<=nums[mid+1]) return mid+1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return mid;
 
-        if(mid+1>=nums.size() && nums[mid]>=nums[mid-1]) return mid;
-        else if(mid+1>=nums.size() && nums[mid]<=nums[mid-1])return mid-1;
-
-        if(nums[mid]>=nums[mid+1] && nums[mid]>=nums[mid-1]) return mid;
-        
-        if(nums[mid]>=nums[mid+1]){
-            high=mid-1;
+            if(nums[mid]<nums[mid+1]) low=mid+1;
+            else high=mid-1;
         }
-        else if(nums[mid]<=nums[mid+1]){
-            low=mid+1;
-        }
-    }
-    return -1;
+        return -1;
 }
 int main(){
     
