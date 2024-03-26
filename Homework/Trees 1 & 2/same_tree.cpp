a6ee24180edb2ab2ee5c56f75944318106ead3cf
Priyansh-Saxena-01 <satyendrasaxena9876@gmail.com>
2024-03-26 19:16:26 +0530

    problem 24 commit

---
+++ Homework/Trees 1 & 2/same_tree.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;   
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(p==nullptr && q==nullptr) return true;
+    
+    if((p==nullptr && q!=nullptr) || (p!=nullptr && q==nullptr)) return false;
+
+    if(p->val!=q->val) return false;
+
+    return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+}
+
+int main(){
+    TreeNode*r= new TreeNode(2);
+
+    r->left= new TreeNode(3);
+    r->right= new TreeNode(10);
+
+    r->left->left= new TreeNode(1);
+    r->left->right= new TreeNode(4);
+
+    r->right->left= new TreeNode(8);
+    r->right->right= new TreeNode(20);
+
+    r->right->left->right= new TreeNode(9);
+    r->left->right->left= new TreeNode(7);
+
+    cout<<isSameTree(r,r->left);
+    return 0;
+}
