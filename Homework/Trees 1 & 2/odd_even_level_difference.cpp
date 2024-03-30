4d40d17297fc91e7c8445ca648030fe30a350ba1
Priyansh-Saxena-01 <satyendrasaxena9876@gmail.com>
2024-03-30 11:53:55 +0530

    problem 32 commit

---
+++ Homework/Trees 1 & 2/odd_even_level_difference.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;   
+
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
+    Node() : val(0), left(nullptr), right(nullptr) {}
+    Node(int x) : val(x), left(nullptr), right(nullptr) {}
+    Node(int x, Node *left, Node *right) : val(x), left(left), right(right) {}
+};
+
+void oddEvenDifference(Node*root,int lvl,pair<int,int>&ans){
+    if(root==nullptr) return;
+
+    if(lvl%2==1) ans.first+=root->val;
+    else ans.second+=root->val;
+
+    oddEvenDifference(root->left,lvl+1,ans);
+    oddEvenDifference(root->right,lvl+1,ans);
+}
+
+int main(){
+    Node*r= new Node(5);
+
+    r->left= new Node(3);
+    r->right= new Node(10);
+
+    r->left->left= new Node(1);
+    r->left->right= new Node(4);
+
+    r->right->left= new Node(8);
+    r->right->right= new Node(20);
+
+    r->right->left->right= new Node(9);
+    r->left->right->left= new Node(7);
+
+    pair<int,int>ans={0,0};
+    oddEvenDifference(r,1,ans);
+    cout<<(ans.first-ans.second);
+    return 0;
+}
