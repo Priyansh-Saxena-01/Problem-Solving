d32ad7588393009304c51c3c053a5246856876fe
Priyansh-Saxena-01 <satyendrasaxena9876@gmail.com>
2024-03-30 12:36:45 +0530

    problem 34 commit

---
+++ Homework/Trees 3/right_side_view_of_BT.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;   
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+void rightView(TreeNode*root,int lvl,vector<int>&taken){
+    if(root==NULL){
+        return;
+    }
+    if(taken.size()==lvl){
+        taken.push_back(root->val);
+    }
+    rightView(root->right,lvl+1,taken);
+    rightView(root->left,lvl+1,taken);
+}
+int main(){
+    TreeNode*r= new TreeNode(2);
+
+    r->left= new TreeNode(3);
+    r->right= new TreeNode(10);
+
+    r->left->left= new TreeNode(1);
+    r->left->right= new TreeNode(4);
+
+    r->right->left= new TreeNode(8);
+    r->right->right= new TreeNode(20);
+
+    r->right->left->right= new TreeNode(9);
+    r->left->right->left= new TreeNode(7);
+
+    vector<int>taken;
+    rightView(r,0,taken);
+    cout<<"right view of the binary tree : "<<endl;
+    for(int i=0;i<taken.size();i++){
+        cout<<taken[i]<<" ";
+    }
+    return 0;
+}
